dffa9fa66d23c295df23e384ecf3baa7bfeb9bf2
ChornyiPavlo <pavlo.chornyi.v@gmail.com>
2024-06-11 11:28:13 +0200

    start

---
+++ feedback.php
@@ -0,0 +1,437 @@
+<?php
+
+$data = implode("\n", $_POST);
+
+$domain = $_SERVER['HTTP_HOST'];
+$to = "lead@".$domain; 
+$subject = "Lead";
+$message = $data;
+$headers = "From: sender@".$domain;
+
+if(mail($to, $subject, $message, $headers)) {
+    //echo "Письмо успешно отправлено!";
+}
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <script type="text/javascript" src="directory/entry_js/jquery-3.7.1.min.js"></script>
+        <title>Glimmerus | অনুরোধ গৃহীত হয়েছে!</title>
+        <meta property="og:title" content="Glimmerus | অনুরোধ গৃহীত হয়েছে!" />
+        <meta property="og:image" content="logocraft.svg"/>
+        
+        <meta property="og:description" content="Glimmerus | অনুরোধ গৃহীত হয়েছে!">
+        <meta name="author" content="Glimmerus | অনুরোধ গৃহীত হয়েছে!"/>
+        <meta name="description" content="Glimmerus | অনুরোধ গৃহীত হয়েছে!">
+       
+        <link rel="stylesheet" type="text/css" href="directory/investments_css/slick.css"/>
+        <link rel="stylesheet" type="text/css" href="directory/investments_css/slick-theme.css"/>
+        <link rel="stylesheet" type="text/css" href="directory/investments_css/bootstrap.css">
+        <link rel="stylesheet" type="text/css" href="directory/investments_css/btn.css">
+        <link rel="stylesheet" type="text/css" href="directory/investments_css/bootstrap-icons.css">
+        <link rel="stylesheet" type="text/css" href="directory/investments_css/lightgallery.css"/>
+        <link  rel="stylesheet" type="text/css" href="directory/investments_css/animate.min.css" />
+
+
+        <script type="text/javascript" src="directory/entry_js/wow.min.js"></script>
+        <script type="text/javascript" src="directory/entry_js/lightgallery.min.js"></script>
+        <script type="text/javascript" src="directory/entry_js/slick.min.js"></script>
+        <script type="text/javascript" src="directory/entry_js/bootstrap.bundle.js"></script> 
+  
+  
+
+        <link rel="shortcut icon" href="logocraft.svg" type="image/x-icon">
+        <link rel="stylesheet" href="common.css">
+        
+        
+
+        <style>
+        
+        </style>
+
+        
+        </head>
+        <body>
+          
+        <script>
+          new WOW().init();
+        </script>
+
+            <section class="section-container-head padding-style">
+                <header class="header navbar padding-style flex-row" id="top">
+                  <div class="container navbar-container">
+      
+                    <a href="./"> 
+                      <div class="header-logo color-text animate__zoomIn">
+                        <img class="img-logo" src="logocraft.svg" >
+                        <div class="headline">Glimmerus</div>
+                      </div>
+                    </a>
+                    
+                    <nav class="navbar-wrap animate__animated animate__zoomIn">
+                      <ul class="nav-list">
+                        <li class="nav-item">
+                          <a class="nav-link" href="./">হোম পেজ</a>
+                        </li>
+
+                        <li class="nav-item dropdown dropdown-expand-lg dropdown-menu-lg-start" id="nav-item">
+                          <a class="nav-link dropdown-toggle" 
+                          href="#"
+                          role="button"
+                          data-bs-toggle="dropdown"
+                          aria-expanded="false">আমাদের সেবা</a>
+                          <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                            
+                            
+                            <li class="dropdown-item">
+                             <a class="drop-link" href="exquisite-wedding-dress-selection-at-subglimmerus-sub.html">Glimmerus এ সূক্ষ্ম বিবাহের পোশাক নির্বাচন</a>
+                          </li>
+                          
+                          
+                          
+                          <li class="dropdown-item">
+                            <a class="drop-link" href="exquisite-custom-tailoring-services-at-subglimmerus-sub-wedding-salon.html">Glimmerus ওয়েডিং সেলুনে সূক্ষ্ম কাস্টম সেলাই পরিষেবা</a>
+                          </li>
+                          
+                          
+                          
+                          <li class="dropdown-item">
+                            <a class="drop-link" href="discover-the-magic-of-your-dream-wedding-at-subglimmerus-sub.html">Glimmerus এ আপনার স্বপ্নের বিবাহের যাদু আবিষ্কার করুন</a>
+                          </li>
+                          
+                          
+                          
+                          <li class="dropdown-item">
+                            <a class="drop-link" href="elegant-wedding-attire-for-your-special-day.html">আপনার বিশেষ দিনের জন্য মার্জিত বিবাহের পোশাক</a>
+                          </li>
+                          
+                          </ul>
+                        </li>
+
+                         
+                        <li class="nav-item">
+                          <a class="nav-link" href="./#ourworker">আমাদের কর্মচারী</a></li>
+                        
+
+                        <li class="nav-item">
+                          <a class="nav-link" href="who-we-are.html">কোম্পানী সম্পর্কে</a></li> 
+
+                        <li class="nav-item">
+                          <a class="nav-link" href="contact-desk.html">যোগাযোগ</a></li> 
+                       
+                      </ul>
+                    </nav>
+                   
+                  </div>
+                </header>
+      
+                  <div class="head-section padding-style img__8Hucg__element">
+                      <div class="container">
+                        <div class="head-title-phone wow animate__animated animate__zoomIn">
+                          <div class="head-title">
+                            <p>Glimmerus-এ আপনাকে স্বাগতম জানাতে পেরে খুশি! এখানে, সবাই সৌন্দর্য এবং শৈলী তাদের পথ খুঁজে পেতে পারেন। একসাথে আমরা এই যাত্রাকে অবিস্মরণীয় করে তুলব।</p>
+                         </div>
+
+                         <div class="head-phone-btn">
+                          
+                         </div>
+                        </div>
+                          
+                      </div>
+                    </div>
+                </section>
+                
+
+
+<style>
+	* {
+		padding: 0;
+		margin: 0;
+	}
+	#mainWrapp-footer-77GIP-section{
+		margin: 0px;
+		padding: 0px;
+		font-family: 'Hind Siliguri', sans-serif;
+		width: 100%;
+		font-size: 18px;
+		padding: 257px 0px;
+	}
+	.bodyClass1-footer-77GIP-section{
+		background: #f5f4f4;
+		color: #ffffff;
+	}
+	.bodyClass2-footer-77GIP-section{
+		background: #c0b9b9;
+		color: #fff;
+	}
+	.bodyClass3-footer-77GIP-section{
+		background: #fff;
+		color: #111;
+	}
+	.wrapage-block-footer-77GIP-section{
+		background-size: 100%;
+		width: 100%;
+	}
+	.box_main-footer-77GIP-section{
+		width: 100%;
+		margin: 0 auto;
+		text-align: center;
+		display: flex;
+		justify-content: center;
+		align-self: center;
+		align-items: center;
+	}
+	.box_main-footer-77GIP-section h2{
+		font-size: 24px;
+		padding: 0px 0px 25px;
+	}
+	.box_main-footer-77GIP-section p{
+		font-weight: 500;
+		font-size: 18px;
+	}
+	p{
+		margin-bottom: 10px;
+	}
+	.mainBlock-footer-77GIP-section{
+		text-align: start;
+	}
+	.mainBlock-footer-77GIP-section ul{
+		text-align: start;
+		padding: 20px;
+		display: flex;
+		flex-direction: column;
+		gap: 15px;
+	}
+	.mainBlock-footer-77GIP-section ul>li span{
+		font-weight: bold;
+	}
+	.mainBlock-footer-77GIP-section{
+		max-width: 964px;
+		margin: 0 auto;
+		padding: 40px;
+		background: #7b7d008c;
+		border-radius: 10px;
+	}
+	.mainBlock-footer-77GIP-section .cBlock-footer-77GIP-section{
+		text-align: start;
+	}
+
+	.bodyClass3-footer-77GIP-section .mainBlock-footer-77GIP-section{
+		background: none;
+		border-top: 2px solid #c0b9b9;
+		border-bottom: 2px solid #c0b9b9;
+	}
+	.bodyClass2-footer-77GIP-section .mainBlock-footer-77GIP-section{
+		background: #272121;
+		color: #fff !important;
+		box-shadow: 0px 0px 10px #272121;
+	}
+	.bodyClass2-footer-77GIP-section .mainBlock-footer-77GIP-section p{
+		color: #fff !important;
+	}
+	.bodyClass1-footer-77GIP-section .mainBlock-footer-77GIP-section{
+		background: #009DC4;
+		color: #ffffff;
+		border-left: 2px solid #03C4A1;
+	}
+	.bodyClass1-footer-77GIP-section .mainBlock-footer-77GIP-section p{
+		color: #ffffff !important;
+	}
+	.order-footer-77GIP-section{
+		font-size: 22px !important;
+	}
+
+	  @media screen and (max-width: 639px) {
+		  .box_main-footer-77GIP-section p{
+			padding: 0px 15px;
+		  }
+		  .box_main-footer-77GIP-section h2{
+			  padding: 0px 10px 15px;
+		  }
+		.mainBlock-footer-77GIP-section{
+			padding: 15px;
+		}
+
+
+	}
+	@media screen and (max-width: 480px) {
+		#mainWrapp-footer-77GIP-section{
+			height: 100%;
+		}
+	}
+</style>
+<div class="bodyClass3-footer-77GIP-section" id="mainWrapp-footer-77GIP-section">
+
+
+	<div class="wrapage-block-footer-77GIP-section">
+		<div class="box_main-footer-77GIP-section">
+			<div class="mainBlock-footer-77GIP-section">
+				<p>আপনার প্রচার এবং আপনি আমাদের উপর যে আস্থা রেখেছেন তার জন্য আমরা সত্যই কৃতজ্ঞ। আপনার সমর্থন আমাদের উত্সর্গীকৃত দলকে ক্রমাগত আমাদের অফারগুলির ক্যালিবার বাড়ানোর ক্ষমতা দেয়।</p>
+<p>মনে রাখবেন, আপনার অন্তর্দৃষ্টি, প্রতিক্রিয়া এবং পরামর্শ আমাদের বৃদ্ধি এবং বিবর্তনের জন্য অমূল্য। যদি আপনার মনে কিছু থাকে বা আপনার যদি সহায়তার প্রয়োজন হয় তবে দয়া করে নির্দ্বিধায় যোগাযোগ করুন। আমাদের প্রতিশ্রুতি আপনাকে সাহায্য করার জন্য সহজেই উপলব্ধ হতে হয়।</p>
+<p class="cBlock-footer-77GIP-section">আন্তরিক ধন্যবাদ এবং উষ্ণ শুভেচ্ছা সহ!</p>
+			</div>
+		</div>
+	</div>
+
+
+</div>
+
+
+
+
+  <section class="footer-contact-section  padding-style flex-row">
+    <div class="footer-logo">
+      <div class="footer-logo-title">
+        <a href="./"> 
+        <div class="header-logo color-text animate__zoomIn">
+          <img class="img-logo" src="logocraft.svg" >
+          <div class="headline">Glimmerus</div>
+        </div>
+      </a>
+    </div>
+      
+
+        
+        <div class="footer-contacts" id="contactid">
+             <ul class="footer-contacts-items">
+                 <li>
+                  <svg width="25" height="25" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path d="M215.7 499.2C267 435 384 279.4 384 192C384 86 298 0 192 0S0 86 0 192c0 87.4 117 243 168.3 307.2c12.3 15.3 35.1 15.3 47.4 0zM192 128a64 64 0 1 1 0 128 64 64 0 1 1 0-128z"/></svg>
+                  <a href="">Dhaka 1205, Bangladesh</a>
+                 </li>
+                 <li>
+                  <svg width="25" height="25" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path d="M16 64C16 28.7 44.7 0 80 0H304c35.3 0 64 28.7 64 64V448c0 35.3-28.7 64-64 64H80c-35.3 0-64-28.7-64-64V64zM224 448a32 32 0 1 0 -64 0 32 32 0 1 0 64 0zM304 64H80V384H304V64z"/></svg>
+                  <a href="tel:+8801315194293"> +8801315194293</a> 
+                 </li>
+                 <li>
+                  <svg  width="25" height="25" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M16.1 260.2c-22.6 12.9-20.5 47.3 3.6 57.3L160 376V479.3c0 18.1 14.6 32.7 32.7 32.7c9.7 0 18.9-4.3 25.1-11.8l62-74.3 123.9 51.6c18.9 7.9 40.8-4.5 43.9-24.7l64-416c1.9-12.1-3.4-24.3-13.5-31.2s-23.3-7.5-34-1.4l-448 256zm52.1 25.5L409.7 90.6 190.1 336l1.2 1L68.2 285.7zM403.3 425.4L236.7 355.9 450.8 116.6 403.3 425.4z"/></svg>
+                  <a href="mailto:info@name.com"> info@name.com</a> 
+                 </li>
+             </ul>
+        </div>
+         
+
+
+       <div class="section-privacy">
+        <div class="footer-rights">
+          <a class="footer-privacy" href="privacyTerms.html" target="_blank">গোপনীয়তা নীতি</a>
+          <a class="footer-privacy" href="userAgreement.html" target="_blank">মেয়াদ ও শর্তাবলী</a>
+          <a class="footer-privacy m-0" href="legal-disclaimer.html" target="_blank">Disclaimers</a>
+        </div>
+      </div>
+    </div>
+
+
+   <div classs="footer-cont-box">
+     <div class="footer-menu-item">
+      <nav class="navbar-wrap">
+        <ul class=" nav-list nav-list-footer">
+          <li class="nav-item">
+            <a class="nav-link-footer footer-size" href="./">হোম পেজ</a>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link-footer footer-size" href="./#ourworker">আমাদের কর্মচারী</a></li>
+            <li class="nav-item">
+              <a class="nav-link-footer footer-size" href="who-we-are.html">কোম্পানী সম্পর্কে</a></li> 
+          <li class="nav-item">
+            <a class="nav-link-footer footer-size" href="contact-desk.html">যোগাযোগ</a></li>
+         
+        </ul>
+      </nav>
+     </div>
+
+     <div>
+      <li class="nav-item navbar-wrap nav-list-footer">
+        <a class="nav-link-footer footer-size" href="#">আমাদের সেবা :</a>
+        <ul class=" nav-list nav-list-footer btn--LbmSL--section" >
+          
+        <li class="nav-item">
+           <a class="nav-link-menu" href="exquisite-wedding-dress-selection-at-subglimmerus-sub.html">Glimmerus এ সূক্ষ্ম বিবাহের পোশাক নির্বাচন</a>
+        </li>
+        
+        
+        
+        <li class="nav-item">
+          <a class="nav-link-menu" href="exquisite-custom-tailoring-services-at-subglimmerus-sub-wedding-salon.html">Glimmerus ওয়েডিং সেলুনে সূক্ষ্ম কাস্টম সেলাই পরিষেবা</a>
+        </li>
+        
+       
+        
+        <li class="nav-item">
+          <a class="nav-link-menu" href="discover-the-magic-of-your-dream-wedding-at-subglimmerus-sub.html">Glimmerus এ আপনার স্বপ্নের বিবাহের যাদু আবিষ্কার করুন</a>
+        </li>
+        
+        
+        
+        <li class="nav-item">
+          <a class="nav-link-menu" href="elegant-wedding-attire-for-your-special-day.html">আপনার বিশেষ দিনের জন্য মার্জিত বিবাহের পোশাক</a>
+        </li>
+        
+        
+        </ul>
+      </li>
+     </div>
+     
+  </div>
+  </section>
+
+  
+  <script>
+        document.addEventListener("DOMContentLoaded", function() {
+        var button = document.querySelector(".pulsating-button");
+        if (button !== null) {
+          button.classList.add("pulsating-animation");
+        } 
+      });
+
+  </script>
+  <script>
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    var images = document.querySelectorAll('.pageIzmen');
+
+    images.forEach(function(img) {
+        var imagesHei = img.naturalHeight;
+        var imagesWid = img.naturalWidth;
+
+        if (imagesHei == imagesWid && imagesWid > 600) {
+            img.style.width = '51%';
+        }
+
+        if (imagesHei > imagesWid) {
+            if (imagesHei < 2800 && imagesHei > 700) {
+                img.style.width = '24%';
+            } else if (imagesHei < 700 && imagesHei > 500) {
+                img.style.width = '52%';
+            }
+        }
+
+        if (imagesHei < imagesWid) {
+            img.style.width = 'auto';
+            if (imagesWid < 600) {
+                img.style.width = '550px';
+            }
+        }
+    });
+});
+
+
+  </script>
+
+
+
+
+    
+
+        
+
+</body>
+</html>
